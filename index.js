3a48a6e814253c41f5d78c2453c85608589c6a3c
Brandon Fisch <brandon.fisch@gmail.com>
2016-01-29 22:16:31 -0800

    added crossplatrom support, updated iOS styles
    now uses PullToRefreshViewAndroid on android devices
    
    uses an arrow character rather than an image
    
    arrow and indicator on iOS are fixed to top of scroll view rather than
    scrolling with content

--- index.js
+++ index.js
@@ -1,3 +1,59 @@
-import PTRView from './lib/PullToRefreshView.js'
-module.exports = PTRView
+'use strict';
 
+var React = require('react-native');
+
+var {
+    Component,
+    Platform,
+    PullToRefreshViewAndroid,
+    } = React;
+
+import PTRView from './lib/PullToRefreshView.js'
+
+
+class PullToRefreshView extends Component {
+
+    constructor () {
+        super();
+        this.state = {
+            isRefreshing: false
+        }
+    }
+
+    _onRefresh(){
+        this.setState({
+            isRefreshing: true,
+        });
+        this.props.onRefresh().done(()=>{
+            this.setState({
+                isRefreshing: false,
+            });
+        })
+    }
+
+    render() {
+        if(Platform.OS == 'android'){
+            return (
+                <PullToRefreshViewAndroid
+                    onRefresh={this._onRefresh.bind(this)}
+                    refreshing={this.state.isRefreshing}
+                    colors={this.props.andoridColors}
+                    style={{flex: 1}}
+                    >
+                    {this.props.children}
+                </PullToRefreshViewAndroid>
+            );
+        } else {
+            return (
+                <PTRView
+                    onRefresh={this._onRefresh.bind(this)}
+                    arrowColor={this.props.iosArrowColor}>
+                    {this.props.children}
+                </PTRView>
+            );
+        }
+    }
+}
+
+
+module.exports = PullToRefreshView;
